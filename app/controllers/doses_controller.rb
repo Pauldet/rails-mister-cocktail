fc2f346b09d1233b56b801c7ed5a5de96e54b9cd
Paul de Thore <p_dethore@hotmail.com>
2020-02-14 15:39:11 +0100

    cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+
+
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
+
+  def create
+    @dose = Dose.new(params_doses)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+private
+  def params_doses
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+
+end
